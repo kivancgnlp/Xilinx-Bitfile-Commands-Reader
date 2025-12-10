d74fb6fa35887697acf56618d74bbd94f7bae495
Kivanc <kivancgunalp@gmail.com>
2025-12-10 14:37:02 +0300

    Unit tests added

--- Bitfile_Reader/src/lib.rs
+++ Bitfile_Reader/src/lib.rs
@@ -1,13 +1,43 @@
+//use IDCODE_Decoder::DecodeData;
+
+
 
 mod IDCODE_Decoder;
+//use IDCODE_Decoder::*;
+
+//use super::IDCODE_Decoder::*;
 
 
+/// Unit tests
 #[cfg(test)]
-#[test]
-fn test_idcode_decode(){
-    let d = IDCODE_Decoder::DecodeData::new();
-    println!("IDCode : {:?}",d.try_to_guess_device_id(0x362d093));
-    println!("IDCode : {:?}",d.try_to_guess_device_id(0x03727093));
-    println!("IDCode : {:?}",d.try_to_guess_device_id(0x04E4093));
+mod tests {
 
+
+
+    use crate::IDCODE_Decoder::DecodeData;
+
+    #[test]
+    fn test_parse_line_methods(){
+
+        //use IDCODE_Decoder::*;
+        //use super::IDCODE_Decoder::DecodeData;
+        let mut result = DecodeData::parse_line_method_1("");
+        assert_eq!(result,None);
+
+        result = DecodeData::parse_line_method_1(" ");
+        assert_eq!(result,None);
+
+        result = DecodeData::parse_line_method_1("# This is a minimal dataset");
+        assert_eq!(result,None);
+
+        result = DecodeData::parse_line_method_1("362D Artix-7 (XC7A35T)");
+        assert_eq!(result,None);
+
+        result = DecodeData::parse_line_method_1("0x362DArtix-7(XC7A35T)");
+        assert_eq!(result,None);
+
+        result = DecodeData::parse_line_method_1("0x362D Artix-7 (XC7A35T)");
+        assert_eq!(result.unwrap(),(0x362D,"Artix-7 (XC7A35T)"));
+
+    }
 }
